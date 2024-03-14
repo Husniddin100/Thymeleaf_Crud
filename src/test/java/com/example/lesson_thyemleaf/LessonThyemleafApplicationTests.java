dfcc7f1df2a1d3b85d086e95ad0d4b139cb2b1f9
Husniddin <uchanal10@gmail.com>
2024-03-14 11:47:05 +0500

    profile init

--- src/test/java/com/example/lesson_thyemleaf/LessonThyemleafApplicationTests.java
+++ src/test/java/com/example/lesson_thyemleaf/LessonThyemleafApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.lesson_thyemleaf;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.parameters.P;
 
 @SpringBootTest
 class LessonThyemleafApplicationTests {
+    @Autowired
+    private ProfileRepository profileRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    void testUpload() {
+        ProfileEntity entity = new ProfileEntity();
+        entity.setName("Ali");
+        entity.setSurname("Aliyev");
+        entity.setPhone("1234");
+        entity.setPassword(MD5Util.getMd5("1"));
+        entity.setRole("ROLE_ADMIN");
+        profileRepository.save(entity);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
